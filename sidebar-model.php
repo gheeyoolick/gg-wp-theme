3e64c181f2ae420195bba39865888df075db6856
Brian Cordyack <brian@cordyack.com>
2017-11-29 16:12:55 -0500

    Make title of Model Home sidebar editable

--- sidebar-model.php
+++ sidebar-model.php
@@ -3,6 +3,7 @@
  * Template for model home sidebar
  */
 
+$model_title = get_field( 'model_title' );
 $model_map_url = get_field( 'model_map_src' );
 $model_address = get_field( 'model_address' );
 $model_details = get_field( 'model_additional_details' );
@@ -10,7 +11,13 @@ $model_details = get_field( 'model_additional_details' );
 if ($model_address) {
 ?>
 <div class="sidebar">
-	<h4>Model Home</h4>
+	<h4><?php
+	if( $model_title ) {
+		echo ( $model_title );
+	} else {
+		echo 'Model Home';
+	}
+	?></h4>
 	<p class="callout icon-directions"><a href="<?php echo esc_html( $model_map_url ); ?>" target="_blank"><?php echo ( $model_address ); ?></a></p>
 	<?php if( $model_details ) {
 		echo wpautop( $model_details );
