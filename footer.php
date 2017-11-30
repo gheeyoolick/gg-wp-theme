f75510803a50b9e31b1632b1cf308229ae777eae
Brian Cordyack <brian@cordyack.com>
2017-11-30 15:28:50 -0500

    Add address and phone number to footer, rework design to accommodate

--- footer.php
+++ footer.php
@@ -4,13 +4,16 @@
 		<div class="container">
 			<div class="row">
 				<nav class="footer-links">
-					 <?php wp_nav_menu( array( 
-						'theme_location' => 'footer',
-						'menu_class'     => '',
-						'container'      => false,
-						'depth'          => 1)
-					); ?>
-					<p class="copyright">&copy; Copyright <?php echo date('Y'); ?> Gulick Group. All Rights Reserved. <span class="eho">Equal Housing Opportunity</span></p>
+					 <div class="text">
+						 <?php wp_nav_menu( array( 
+							'theme_location' => 'footer',
+							'menu_class'     => '',
+							'container'      => false,
+							'depth'          => 1)
+						); ?>
+						<p class="address">11790 Sunrise Valley Drive, Suite 225<br>Reston, Virginia 20191<br>703.674.0330</p>
+						<p class="copyright">&copy; Copyright <?php echo date('Y'); ?> Gulick Group. All Rights Reserved. <span class="eho">Equal Housing Opportunity</span></p>
+					</div>
 				</nav>
 				<div class="footer-script">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/gulick.png" alt="Gulick" class="script-img">
